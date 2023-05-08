8e3d0c315d646fe50414eff0161706fe0ba8e198
selahjs <sjemal99@gmail.com>
2023-05-08 13:59:53 +0300

    made homepage get data from mockoon api, postponed modal fixing

--- src/Pages/Admin.jsx
+++ src/Pages/Admin.jsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react'
 import Table from '../components/Table'
 import ModalC from '../components/ModalC';
 import Button from "../components/Button";
+import NewModal from '../components/NewModal';
 import data from '../api/data'
 
 const Admin = () => {
@@ -23,7 +24,9 @@ const Admin = () => {
     <>
       {/* the admin controls the add product button as well as the edit button on the table */}
       <Button onClick={onClick} buttonName="Add Products"/>
+      
       {show && <ModalC show={show} onClick={onClick} onClose={onClose} buttonName="Add Products"/>}
+      {/* <NewModal /> */}
       
       <Table onClick={onClick} show={show} onClose={onClose} data={tableData}/>
     </>
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -3,28 +3,31 @@ import { Spinner, Button } from "flowbite-react";
 
 import Hero from "../components/Hero";
 import Card from "../components/Cards";
-import data from "../api/data";
+// import data from "../api/data";
 
 const HomePage = () => {
   //we get products from local storage
   const [products, setProducts] = useState([]);
 
   useEffect(()=>{
-    setProducts(getData())
-  }, [data])
-
+    getData()
+  
+  }, [products])
+  
+  function getData(){
+    fetch('http://localhost:3002/')
+      .then(res => res.json())
+      .then(data => {
+        setProducts(data)
+        localStorage.setItem("products", JSON.stringify(data))
+      })
+  }
+  
   const [loading, setLoading] = useState(true);
   setTimeout(() => {
     setLoading(false);
   }, 100);
 
-  function getData(){
-    // fetch goes here
-    // for now we are getting data from localStorage
-    // localStorage.setItem("products", JSON.stringify(data))
-    const productData = JSON.parse(localStorage.getItem("products"));
-    return productData
-  }
 
   const productElemnts = products.map((product)=>(
     <Card key={product.id} data={product}/>
